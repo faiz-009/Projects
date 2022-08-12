9a13bb7b3225604aa3c70014c1b6dcd4024edbcf
MOHD. FAIZ QURESHI <mdfaizqureshi09@gmail.com>
2022-08-12 23:47:25 +0530

    Update main.py

--- VIRTUAL ASSITANT/main.py
+++ VIRTUAL ASSITANT/main.py
@@ -1,60 +1,78 @@
-import speech_recognition as sr
 import pyttsx3
-import pywhatkit
 import datetime
-import wikipedia
-import pyjokes
+import speech_recognition as sr
 
-listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+# print(voices[0].id)
+# print(voices[1].id)
+# print(voices[2].id)
+# print(voices[3].id)
+# print(voices[4].id)
+# print(voices[5].id)
 
+engine.setProperty('voice', voices[4].id)
 
-def talk(text):
-    engine.say(text)
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
 
-
-def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
-    except:
-        pass
-    return command
-
-
-def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >=0 and hour<12 :
+        speak("Good Morning Faaiz, I am Alexa. ")
+    elif hour >=12 and hour<16:
+        speak("Good afternoon Faaiz, I am Alexa. ")
     else:
-        talk('Please say the command again.')
+        speak("Good evening sir, I am Jarvis. ")
+    
+def intro():
+    intro = 'My name is Jarvis. I am an artificial intelligence, developed by  Mohammad Faaiz .'
+    speak(intro)
 
+def exit():
+    speak("Thank you sir, have a good day")
 
-while True:
-    run_alexa()
+def runAlexa():
+    
+    speak("How can I help you ?")
+    command = takeCommand()
+
+    if 'youtube'.lower() in command:
+        #youTube()
+        pass
+    elif 'who are you' in command:
+        intro()
+    elif 'go' in command:
+        exit()
+        return -1
+
+    
+
+def takeCommand():
+    '''It takes audio input from and returns string output'''
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.....")
+        #r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language = 'en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        #print(e)
+        print("Say that again please....")
+        return "None"
+    return query
+        
+
+if __name__ == '__main__':
+    #code
+    print("Speeking.....")
+    wishMe()
+    while 1:
+        a = runAlexa()
+        if a==-1:
+            break
