2487c7b4f7ad8aca9d54e253672ba6d7a0f3efd1
MOHD. FAIZ QURESHI <mdfaizqureshi09@gmail.com>
2022-08-14 02:11:44 +0530

    Update tempCodeRunnerFile.py

--- VIRTUAL ASSITANT/tempCodeRunnerFile.py
+++ VIRTUAL ASSITANT/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-jarvi
+contacts = {'faiz':'mdfaizqureshi09@gmail.com',
+                'mayur': 'en19cs301197@medicaps.ac.in',
+               }
+query = 'faiz'
+if query in contacts:
+    print("yes")
